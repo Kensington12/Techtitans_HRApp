9f5f07cc9096397197e05f7df64756a270b196a4
Austin Bell <144272907+Aust1nB311@users.noreply.github.com>
2024-03-29 12:52:46 -0500

    Temp Array file
    Class for creating array lists so we can have fake values for the demo

---
+++ TempArrays.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TempArrays 
+{
+    private ArrayList<String> userNames = new ArrayList<String>();
+    private ArrayList<Integer> ageInYears = new ArrayList<Integer>();
+    private ArrayList<String> phoneNumbers = new ArrayList<String>();
+    private ArrayList<String> userAddress = new ArrayList<String>();
+
+}
