6785e3516e5b9074148e0b47f56da688e0a7d4aa
Jolie Barger <jolie.barger@gmail.com>
2024-05-01 13:31:28 -0500

    Current classes

--- HRAPP/Evaluation.java
+++ HRAPP/Evaluation.java
@@ -69,4 +69,76 @@ public class Evaluation extends tempArray{
         setNotes(scn.nextLine());
         addArray(new String[] {empName, evalSupervisor, evalDate, empMentalState, notes});
     }
+
+    public void editAccountInformation() {
+        System.out.println("Enter the employee's system ID: ");
+        int arrayIndex = Integer.parseInt(scn.nextLine());
+            int choice = 2; // just something other than 0
+            while (choice != 0) {
+                System.out.println("1. Edit name");
+                System.out.println("2. Edit evaluating supervisor");
+                System.out.println("3. Edit evaluation date");
+                System.out.println("4. Change employee mental status");
+                System.out.println("5. Change notes");
+                System.out.println("0. Exit");
+                System.out.print("Enter choice: ");
+                choice = Integer.parseInt(scn.nextLine());
+                switch (choice) {
+                    case 1:
+                        System.out.println("Enter name: ");
+                        setEmpName(scn.nextLine());
+                        editItemInArray(arrayIndex, 0, empName);
+                        break;
+                    case 2:
+                        System.out.print("Enter evaluating supervisor : ");
+                        setEvalSupervisor(scn.nextLine());
+                        editItemInArray(arrayIndex, 1, evalSupervisor);
+                        break;
+                    case 3:
+                        System.out.print("Enter evaluation date: ");
+                        setEvalDate(scn.nextLine());
+                        editItemInArray(arrayIndex, 2, evalDate);
+                        break;
+                    case 4:
+                        System.out.print("Enter employee mental status: ");
+                        setEmpMentalState(scn.nextLine());
+                        editItemInArray(arrayIndex, 3, empMentalState);
+                        break;
+                    case 5:
+                        System.out.print("Enter notes: ");
+                        setNotes(scn.nextLine());
+                        editItemInArray(arrayIndex, 4, notes);
+                        break;
+                    case 0:
+                        System.out.println("Exiting edit-menu...");
+                        break;
+                }
+            }
+        }
+        public void displayEval() {
+            String[] titles = {"Name: ", "Evaluating Supervisor: ", "Evaluation Date: ", "Employee Mental State: ", "Notes: "};
+            System.out.println("Enter the employee's system ID: ");
+            int arrayIndex = Integer.parseInt(scn.nextLine());
+            if (arrayIndex >= 0 && arrayIndex < arrays.size()) {
+                Object[] array = arrays.get(arrayIndex);
+                System.out.println("Items for employee " + arrayIndex + ": ");
+                for (int i = 0; i < array.length; i++) {
+
+                    System.out.println(titles[i] + array[i] + " ");
+                }
+                System.out.println();
+            } else {
+                System.err.println("Employee not found");
+            }
+        }
+        public void deleteEval() {
+            System.out.print("Enter the employee's system ID: ");
+            int arrayIndex = Integer.parseInt(scn.nextLine());
+                if (arrayIndex >= 0 && arrayIndex < arrays.size()) {
+                    arrays.remove(arrayIndex);
+                    System.out.println("The evaluation has been deleted!");
+                } else {
+                    System.err.println("Evaluation not found");
+                }
+            }
 }
